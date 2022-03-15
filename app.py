f217049b2d2b3fde00449537ce8b0be67756a74e
Jorge Massih <jorgmassih@gmail.com>
2022-03-15 18:18:52 -0400

    ref: divides main class in two class
    A class called "PortainerAPIConsumer" was added to better mantainability and scalability.

--- app.py
+++ app.py
@@ -7,16 +7,10 @@ from urllib3.exceptions import InsecureRequestWarning
 from utils.utils import *
 
 
-class PortainerDeployer:
-    """Manage Portainer's Stacks usgin its API.
-    """
-    def __init__(self):
-        """Initialize the PortainerDeployer class and runs the main function.
-        """        
-        # Load .env file
-        env_file = configparser.ConfigParser()
-        env_file.read('.env')
-        PATH_TO_CONFIG = env_file['CONFIG']['PATH_TO_CONFIG']
+class PortainerAPIConsumer:
+    
+    def __init__(self, path_to_config: str):
+        PATH_TO_CONFIG = path_to_config
 
         # Load config
         config = configparser.ConfigParser()
@@ -28,9 +22,95 @@ class PortainerDeployer:
             # Suppress only the single warning from urllib3 needed.
             requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
-        # Set portainer connection
-        self.__portainer_connection = f"{self._portainer_config['URL']}:{self._portainer_config['PORT']}" 
+        # Set portainer connection parameters
+        self.__portainer_connection_str = f"{self._portainer_config['URL']}:{self._portainer_config['PORT']}" 
         self.__connection_headers = {'X-API-Key': self._portainer_config['TOKEN']}
+
+
+    def get_stack(self, name:str=None, stack_id:int=None):
+        """_summary_: Get a stack from portainer
+
+        Args:
+            name (str, optional): Name of the stack in Portainer. Defaults to None.
+            stack_id (int, optional): Id of the stack in Portainer. Defaults to None.
+        """
+        r = requests.get(
+            f"{self.__portainer_connection_str}/api/stacks", 
+            headers=self.__connection_headers,
+            verify=False
+        )
+        data = r.json()
+
+        spacing_str = '{0:<5} {1:<12} {2:<30} {3:30} {4:<30}'
+        print(spacing_str.format('Id', 'Endpoint Id', 'Name', 'Creation', 'Last Updated'))
+
+        data = format_stack_list(data)
+        
+        # Return all stacks if name and id aren't set
+        if not name and not stack_id:
+            for stack in data:
+                print(spacing_str.format(*stack))
+        
+        # Return a specific stack given by the name
+        elif name and not stack_id:
+            for stack in data:
+                if stack[2] == name:
+                    print(spacing_str.format(*stack))
+        
+        # Return a specific stack given by the stack id
+        else:
+            for stack in data:
+                if stack[0] == stack_id:
+                    print(spacing_str.format(*stack))
+
+
+    def post_stack_from_str(self, stack: str, endpoint_id: int, name: str = generate_random_hash()):
+       #TODO: Implement
+       pass
+
+
+    def post_stack_from_file(self, path: str, endpoint_id: int, name: str = generate_random_hash()):
+        # Open file
+        with open(path, 'r') as f:
+            form_data = {
+                'Name': name
+            }
+            
+            params = {
+                "type": 2,
+                "endpointId": endpoint_id,
+                "method": "file"
+            }
+            
+            r = requests.post(self.__portainer_connection_str + '/api/stacks',
+                 data=form_data, 
+                 params=params,
+                 files={'file': f},
+                 headers=self.__connection_headers, 
+                 verify=False
+            )
+            print(r.status_code, r.text)
+    
+
+    def update_stack(self, stack_id: str, stack: str):
+        #TODO: Implement
+        pass
+
+
+class PortainerDeployer:
+    """Manage Portainer's Stacks usgin its API.
+    """
+    def __init__(self):
+        """Initialize the PortainerDeployer class and runs the main function.
+        """        
+        # Load .env file
+        env_file = configparser.ConfigParser()
+        env_file.read('.env')
+        PATH_TO_CONFIG = env_file['CONFIG']['PATH_TO_CONFIG']
+
+
+        # Set API consummer object
+        self.api_consumer = PortainerAPIConsumer(path_to_config=PATH_TO_CONFIG)
         
         # Set arguments
         self._main_parser, self._get_parser, self._deploy_parser, = self.__parser()
@@ -143,18 +223,18 @@ class PortainerDeployer:
         return parser, parser_get, parser_deploy
 
 
-    def __get_sub_command(self , args: argparse.Namespace, parser: argparse.ArgumentParser):
+    def __get_sub_command(self , args: argparse.Namespace, parser: argparse.ArgumentParser) -> None:
         if args.all:
-            self.get_stack()
+            self.api_consumer.get_stack()
         else:
-            self.get_stack(name=args.name, stack_id=args.id)
+            self.api_consumer.get_stack(name=args.name, stack_id=args.id)
 
 
-    def __deploy_sub_command(self, args: argparse.Namespace, parser: argparse.ArgumentParser):
+    def __deploy_sub_command(self, args: argparse.Namespace, parser: argparse.ArgumentParser) -> None:
         if args.stack:
             if args.modify_stack:
                 parser.error('You can not use "--modify-stack" argument with "--stack" argument. It is only available for "--path" argument.')
-            self.post_stack_from_string(stack=args.stack, endpoint_id=args.endpoint)
+            self.api_consumer.post_stack_from_str(stack=args.stack, endpoint_id=args.endpoint)
         
         elif args.path:
             if not os.path.isfile(args.path):
@@ -169,76 +249,9 @@ class PortainerDeployer:
                         if edited:
                             parser.error(edited)
 
-            self.post_stack_from_file(path=args.path, endpoint_id=args.endpoint)
+            self.api_consumer.post_stack_from_file(path=args.path, endpoint_id=args.endpoint)
 
 
-    def get_stack(self, name:str=None, stack_id:int=None):
-        """_summary_: Get a stack from portainer
-
-        Args:
-            name (str, optional): Name of the stack in Portainer. Defaults to None.
-            stack_id (int, optional): Id of the stack in Portainer. Defaults to None.
-        """
-        r = requests.get(
-            f"{self.__portainer_connection}/api/stacks", 
-            headers=self.__connection_headers,
-            verify=False
-        )
-        data = r.json()
-
-        spacing_str = '{0:<5} {1:<12} {2:<30} {3:30} {4:<30}'
-        print(spacing_str.format('Id', 'Endpoint Id', 'Name', 'Creation', 'Last Updated'))
-
-        data = format_stack_list(data)
-        
-        # Return all stacks if name and id aren't set
-        if not name and not stack_id:
-            for stack in data:
-                print(spacing_str.format(*stack))
-        
-        # Return a specific stack given by the name
-        elif name and not stack_id:
-            for stack in data:
-                if stack[2] == name:
-                    print(spacing_str.format(*stack))
-        
-        # Return a specific stack given by the stack id
-        else:
-            for stack in data:
-                if stack[0] == stack_id:
-                    print(spacing_str.format(*stack))
-
-
-    def post_stack_from_str(self, stack: str):
-       pass
-
-
-    def post_stack_from_file(self, path: str, endpoint_id: int, name: str = generate_random_hash()):
-        # Open file
-        with open(path, 'r') as f:
-            form_data = {
-                'Name': name
-            }
-            
-            params = {
-                "type": 2,
-                "endpointId": endpoint_id,
-                "method": "file"
-            }
-            
-            r = requests.post(self.__portainer_connection + '/api/stacks',
-                 data=form_data, 
-                 params=params,
-                 files={'file': f},
-                 headers=self.__connection_headers, 
-                 verify=False
-            )
-            print(r.status_code, r.text)
-    
-
-    def update_stack(self, stack_id: str, stack: str):
-        #TODO: Implement
-        pass
 
 
 if __name__ == '__main__':
