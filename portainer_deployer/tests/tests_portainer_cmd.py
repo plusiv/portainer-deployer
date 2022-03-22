d60b08ca2f486d72344cba48c1e1be6eb40198ee
Jorge Massih <jorgmassih@gmail.com>
2022-03-22 19:25:53 -0400

    test(cmd): adds more tests

--- portainer_deployer/tests/tests_portainer_cmd.py
+++ portainer_deployer/tests/tests_portainer_cmd.py
@@ -1,7 +1,7 @@
 import unittest
 from app import PortainerDeployer
 
-class PortinaerCMDTest(unittest.TestCase):
+class PortainerCMDTest(unittest.TestCase):
 
     # ================================ Test Helps ================================
     def test_global_help(self):
@@ -35,12 +35,26 @@ class PortinaerCMDTest(unittest.TestCase):
 
         self.assertEqual(cm.exception.code, 0)
 
+
+    # ================================ Test Defualt functions in sub-commands ================================
+
     def test_get_subcommand(self):
         tester = PortainerDeployer()
         args = tester.parser.parse_args(['get'])
-        print(vars(args))
 
-        #self.assertEqual(cm.exception.code, 0)
+        self.assertEqual(args.func.__name__, '__get_sub_command')
+
+    def test_deploy_subcommand(self):
+        tester = PortainerDeployer()
+        args = tester.parser.parse_args(['deploy'])
+
+        self.assertEqual(args.func.__name__, '__deploy_sub_command')
+    
+    def test_config_subcommand(self):
+        tester = PortainerDeployer()
+        args = tester.parser.parse_args(['config'])
+
+        self.assertEqual(args.func.__name__, '__config_sub_command')
 
 if __name__ == '__main__':
     unittest.main()
