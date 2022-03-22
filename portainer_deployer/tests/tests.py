6f52217a6b67821802e0ca59bf21a7100dfeb98e
Jorge Massih <jorgmassih@gmail.com>
2022-03-22 18:10:00 -0400

    test: adds helps tests

--- portainer_deployer/tests/tests.py
+++ portainer_deployer/tests/tests.py
@@ -1,16 +1,46 @@
 import unittest
-from app import PortainerDeployerTest
+from app import PortainerDeployer
 
 class PortinaerCMDTest(unittest.TestCase):
 
-
-    def test_portinaer_cmd_help(self):
-        tester = PortainerDeployerTest()
+    # ================================ Test Helps ================================
+    def test_global_help(self):
+        tester = PortainerDeployer()
         with self.assertRaises(SystemExit) as cm:
             tester.parser.parse_args(['--help'])
 
         self.assertEqual(cm.exception.code, 0)
 
+        
+    def test_get_help(self):
+        tester = PortainerDeployer()
+        with self.assertRaises(SystemExit) as cm:
+            tester.parser.parse_args(['get', '--help'])
+
+        self.assertEqual(cm.exception.code, 0)
+
+
+    def test_deploy_help(self):
+        tester = PortainerDeployer()
+        with self.assertRaises(SystemExit) as cm:
+            tester.parser.parse_args(['deploy', '--help'])
+
+        self.assertEqual(cm.exception.code, 0)
+
+
+    def test_config_help(self):
+        tester = PortainerDeployer()
+        with self.assertRaises(SystemExit) as cm:
+            tester.parser.parse_args(['config', '--help'])
+
+        self.assertEqual(cm.exception.code, 0)
+
+    def test_get_subcommand(self):
+        tester = PortainerDeployer()
+        args = tester.parser.parse_args(['get'])
+        print(vars(args))
+
+        #self.assertEqual(cm.exception.code, 0)
 
 if __name__ == '__main__':
     unittest.main()
