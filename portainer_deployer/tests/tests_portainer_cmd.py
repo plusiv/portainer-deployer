e3f5b05d2549e69fda26d26e13e9b879ed4ff461
Jorge Massih <jorgmassih@gmail.com>
2022-03-23 14:04:38 -0400

    test: adds mock in testing

--- portainer_deployer/tests/tests_portainer_cmd.py
+++ portainer_deployer/tests/tests_portainer_cmd.py
@@ -1,5 +1,17 @@
 import unittest
+from unittest.mock import Mock
 from app import PortainerDeployer
+
+
+class PortainerDeployerTest(PortainerDeployer):
+    def __init__(self):
+        super().__init__()
+        
+        self.create_api_consumer_mock() 
+
+    def create_api_consumer_mock(self):
+        self.api_consumer = Mock()
+
 
 class PortainerCMDTest(unittest.TestCase):
 
@@ -55,6 +67,10 @@ class PortainerCMDTest(unittest.TestCase):
         args = tester.parser.parse_args(['config'])
 
         self.assertEqual(args.func.__name__, '__config_sub_command')
+    
+    # ================================ Test API Consumer ================================
+    def test_get_stack(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
