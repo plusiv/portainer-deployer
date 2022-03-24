b877420763eb56a4759a206e194d08727b0a5499
Jorge Massih <jorgmassih@gmail.com>
2022-03-24 14:22:44 -0400

    test(cmd): add tests

--- portainer_deployer/tests/tests_portainer_cmd.py
+++ portainer_deployer/tests/tests_portainer_cmd.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest.mock import Mock, MagicMock
+from random import randint
 
 
 from app import PortainerDeployer
@@ -21,15 +22,14 @@ class PortainerCMDTest(unittest.TestCase):
         args = tester.parser.parse_args(['get'])
         self.assertEqual(args.func, tester._get_sub_command)
 
-
     def test_deploy_subcommand(self):
-        tester = PortainerDeployer()
+        tester = PortainerDeployerTest()
         args = tester.parser.parse_args(['deploy'])
 
         self.assertEqual(args.func, tester._deploy_sub_command)
-    
+
     def test_config_subcommand(self):
-        tester = PortainerDeployer()
+        tester = PortainerDeployerTest()
         args = tester.parser.parse_args(['config'])
 
         self.assertEqual(args.func, tester._config_sub_command)
@@ -41,11 +41,26 @@ class PortainerCMDTest(unittest.TestCase):
         args.func(args)
         tester.api_consumer.get_stack.assert_called_once()
 
-    def test_get_stack(self):
+    def test_get_stack_by_id(self):
         tester = PortainerDeployerTest()
-        args = tester.parser.parse_args(['get', '--all'])
+
+        stack_id = randint(1, 100)
+        cmd_args = ['get', '--id', str(stack_id)]
+        
+        args = tester.parser.parse_args(cmd_args)
         args.func(args)
-        tester.api_consumer.get_stack.assert_called_once()
+        tester.api_consumer.get_stack.assert_called_once_with(name=None, stack_id=stack_id)
+
+    def test_get_stack_by_name(self):
+        tester = PortainerDeployerTest()
+
+        stack_name = 'random_stack_name'
+        cmd_args = ['get', '--name', stack_name]
+        
+        args = tester.parser.parse_args(cmd_args)
+        args.func(args)
+        tester.api_consumer.get_stack.assert_called_once_with(name=stack_name, stack_id=None)
+
 
 if __name__ == '__main__':
     unittest.main()
