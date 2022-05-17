84f149e6f39f28e48f4e051621790034bd6bd975
Jorge Massih <jorgmassih@gmail.com>
2022-05-17 15:41:35 -0400

    chore: customizes defautl help message

--- portainer_deployer/__init__.py
+++ portainer_deployer/__init__.py
@@ -7,5 +7,6 @@ info = load(open(path.join(path.abspath(path.dirname(__file__)), 'info.json')))
 VERSION = info['version']
 PHASE = info['phase']
 PROG = info['info']['prog']
+DEFAULT_HELP_MESSAGE = 'Show help message and exit.'
 
 __all__ = ['config', 'utils']
